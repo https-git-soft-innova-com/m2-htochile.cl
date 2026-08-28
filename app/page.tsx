2772f7a8a65e77a7e69e16f4e48cb7a9578ab902
Felipe Ahumada A <felipe.ahumada.araya@gmail.com>
2026-08-27 21:27:25 -0400

    feat: agregar branding prensso y banner parallax al hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { HomeHero } from "@/components/home/home-hero"
 import { HomeBenefits } from "@/components/home/home-benefits"
 import { BrandsMarquee } from "@/components/home/brands-marquee"
 import { HomeIndustries } from "@/components/home/home-industries"
+import { HomeTransition } from "@/components/home/home-transition"
 import { HomeProducts } from "@/components/home/home-products"
 import { HomeTeam } from "@/components/home/home-team"
 import { HomeDocCenter } from "@/components/home/home-doc-center"
@@ -16,6 +17,7 @@ export default function HomePage() {
       <BrandsMarquee />
       <HomeBenefits />
       <HomeIndustries />
+      <HomeTransition />
       <HomeProducts />
       <HomeTeam />
       <HomeDocCenter />
---
+++ components/home/home-transition.tsx
@@ -0,0 +1,13 @@
+export function HomeTransition() {
+  return (
+    <section
+      aria-label="Representación exclusiva PRENSSO"
+      className="relative h-[260px] overflow-hidden bg-cover bg-center bg-fixed sm:h-[340px] lg:h-[420px]"
+      style={{ backgroundImage: "url('/images/slider-referencia.png')" }}
+    >
+      <span className="sr-only">
+        PRENSSO Máquinas, representantes exclusivos en Chile
+      </span>
+    </section>
+  )
+}
